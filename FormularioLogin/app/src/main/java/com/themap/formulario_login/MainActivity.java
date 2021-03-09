a969a9375952f9f2b45f2d0d6c35b42191d0bc5a
Mauricio Aviles Piña <mauavilespi@outlook.com>
2021-03-09 17:41:38 -0600

    Formulario Completo
    El formulario regresa los datos escritos en un Toast.

--- FormularioLogin/app/src/main/java/com/themap/formulario_login/MainActivity.java
+++ FormularioLogin/app/src/main/java/com/themap/formulario_login/MainActivity.java
@@ -3,6 +3,9 @@ package com.themap.formulario_login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Objects;
 
@@ -15,6 +18,25 @@ public class MainActivity extends AppCompatActivity {
 
         //Quitar actionBar
         Objects.requireNonNull(this.getSupportActionBar()).hide();
+    }
+
+    //Definir el método que va a asociarse al onClick del Button
+    // y desplegar un Toast en la pantalla del dispositivo móvil.
+    public void onClickButton(View view) {
+        //Recuperar el email y password escritos.
+        EditText editTextUsuario = this.findViewById(R.id.editTextUsuario);
+        EditText editTextPassword = this.findViewById(R.id.editTextPassword);
+        String strEmail = editTextUsuario.getText().toString();
+        String strPassword = editTextPassword.getText().toString();
+        String strMensaje = "Bienvenido: \n" + "Usuario: " + strEmail + "\n" +
+                "Password: " + strPassword;
+
+        //Mostrar el toast correspondiente.
+        Toast.makeText(this, strMensaje, Toast.LENGTH_LONG).show();
+
+        //Abrevieando recuperar el password,
+        // String strPassword = ((EditText)findViewById(R.id.editTextPassword)).getText().toString();
 
     }
 }
+
